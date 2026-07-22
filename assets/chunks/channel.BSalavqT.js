e385bf7ae5412471e2f2a611213d1fe063411eeb
heihay <heihay@users.noreply.github.com>
2026-07-22 09:34:07 +0000

    deploy: 72f7a2590af95ac866efd8fdf8ae29a20b352f12

--- assets/chunks/channel.BSalavqT.js
+++
@@ -1,1 +0,0 @@
-import{U as a,C as n}from"../app.BuSA894v.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
---
+++ assets/chunks/swimlanesDiagram-G3AALYLV.BS0y_HKC.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.Co-JGeub.js";import{_ as a}from"../app.W8M1xyz5.js";import"./chunk-5VM5RSS4.D9YRQ5wB.js";import"./chunk-XXDRQBXY.Dgh1Dt4Z.js";import"./chunk-VR4S4FIN.JQgzq8Qy.js";import"./chunk-32BRIVSS.CDftxEOo.js";import"./channel.bJKTd3ba.js";import"./framework.DPTpzYjp.js";import"./theme.BLQ5_t7l.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
