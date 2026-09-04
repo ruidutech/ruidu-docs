e437f15193c7e72566cb61fe8abeaa8733f4cafc
heihay <heihay@users.noreply.github.com>
2026-09-04 06:59:22 +0000

    deploy: c127853188e34eb98f13fb06c5fcb1ebfa8a1653

---
+++ assets/chunks/swimlanesDiagram-VR7AAH4N.Bu0Fuez1.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW.Bfms8poK.js";import{_ as a}from"../app.CnSlo-Lo.js";import"./chunk-5VM5RSS4.DFgtqX0H.js";import"./chunk-XXDRQBXY.Dw7c0Dap.js";import"./chunk-POPQ4Y6H.C5IQ-xPC.js";import"./chunk-F27PBJKO.BMfe7u-D.js";import"./channel.BVPopkf9.js";import"./framework.Bn19FVGY.js";import"./theme.CZ4gF7ow.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
